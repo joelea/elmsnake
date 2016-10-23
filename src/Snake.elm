f006f96ed03f01c0678922750231e457a5bf8787
Joe Lea <joeleapublic@gmail.com>
2016-10-23 20:55:35 +0100

    Get it going!

--- src/Snake.elm
+++ src/Snake.elm
@@ -5,11 +5,22 @@ import Html.App as Html
 import Html.Attributes exposing (..)
 import Models exposing (..)
 import ViewBoard exposing (..)
+import Keyboard
 
 main : Program Never
-main = Html.beginnerProgram { model = model, view = view, update = update }
+main = Html.program { init = ( model, Cmd.none ), view = view, update = update, subscriptions = subscriptions }
 
-type Msg = Noop | AnotherNoop
+up = 38
+
+filterDowns : Keyboard.KeyCode -> Msg
+filterDowns keyCode = if keyCode == up then Tick else Noop
+
+subscriptions : GameState -> Sub Msg
+subscriptions model = Sub.batch
+    [ Keyboard.presses filterDowns
+    ]
+
+type Msg = Noop | Tick
 
 model : GameState
 model =
@@ -18,5 +29,20 @@ model =
     , snake = [ Position 2 2, Position 2 3 ]
     }
 
-update : Msg -> GameState -> GameState
-update msg state = state
+update : Msg -> GameState -> ( GameState, Cmd Msg )
+update msg state =
+    case msg of
+        Tick -> moveSnakeForward state ! []
+        Noop -> state ! []
+
+moveUp position = { position | y = position.y + 1 }
+
+moveSnakeForward : GameState -> GameState
+moveSnakeForward state =
+    let
+        snakeLength = List.length state.snake
+        oldHead = Maybe.withDefault (Position 0 0) <| List.head state.snake
+        newHead = moveUp oldHead
+        newSnake = List.take snakeLength <| newHead :: state.snake
+    in
+    { state | snake = newSnake }
